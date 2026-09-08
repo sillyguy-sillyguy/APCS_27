55de56cc4bfe53c8a14478bc929b05d5e824e657
sillyguy-sillyguy <asan8563@stu.gusd.net>
2026-09-08 20:40:41 +0000

    hi

--- 000_Labs/006_doubles/base_code/starter.java
+++ 000_Labs/006_doubles/base_code/starter.java
@@ -9,6 +9,8 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Double Celsius=33.8; 
+		int F=100;
+		System.out.println("The Celsius value for "+F+" degrees Fahrenheit is "+(F/Celsius)+" degrees.");
 	}
 }
