0334481f49615a90f91aad0b694939e18591eb5d
sillyguy-sillyguy <asan8563@stu.gusd.net>
2026-09-09 20:56:11 +0000

    HOW MANY BRO

--- 102_Exercises/08_ErrorPractice/error.java
+++ 102_Exercises/08_ErrorPractice/error.java
@@ -1,15 +1,16 @@
 class error {
 	public static void main(String args[]) {
         
-		system.out.println("Happy vacation day back!");
-        system.out.println("What'd you do this weekend? I watched a movie!");
+		System.out.println("Happy vacation day back!");
+        System.out.println("What'd you do this weekend?");
+		System.out.println("I watched a movie!");
 
-		int new number = 125;
+		int new number count = 125;
 		System.out.println(count);
 
-		int num1 = 12.2;
-		int num2 = 14.3;
-		answer = num1 * num2 + answer;
+		double num1 = 12.2;
+		double num2 = 14.3;
+		double answer = num1 * num2 + answer;
 		
 		System.out.println(answer);
 
