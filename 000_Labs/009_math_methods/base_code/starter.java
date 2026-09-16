881c7fa92a36ebc47f720b0ce85b5493d7143988
sillyguy-sillyguy <asan8563@stu.gusd.net>
2026-09-16 20:40:32 +0000

    where mortals fall

--- 000_Labs/009_math_methods/base_code/starter.java
+++ 000_Labs/009_math_methods/base_code/starter.java
@@ -7,8 +7,6 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		Scanner sc = new Scanner();
+		
 	}
 }
