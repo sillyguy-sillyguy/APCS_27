d15eed2774304eb5f53df66b0cfb2b142c36d7f6
sillyguy-sillyguy <asan8563@stu.gusd.net>
2026-09-17 20:31:28 +0000

    what lies beyond the night?

--- 000_Labs/009_math_methods/base_code/starter.java
+++ 000_Labs/009_math_methods/base_code/starter.java
@@ -7,7 +7,10 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		static double = pow(3.1,4.0);
-		
+		Math.max(1,3);
+		System.out.println("Maximum = "+Math.max(13 - 6 * 11, 30 % 7 * (-2)));
+		System.out.println("Square root = "+Math.sqrt((3 * 8 + 31 % 7)));
+		System.out.println("Power = "+Math.pow((37 / 3),( 35 % 21)));
+		System.out.println("Max = "+Math.max( Math.pow( 2,14%3) , Math.sqrt ( 2*6) ));
 	}
 }
--- 000_Labs/010_random_method-static/base_code/starter.java
+++ 000_Labs/010_random_method-static/base_code/starter.java
@@ -8,8 +8,13 @@ import java.util.Random;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		int x = (int)(Math.random()*10);
+		System.out.println("A number between 0-9: "+x);
+		int y = (int)(Math.random()*11);
+		System.out.println("A number between 0-10: "+y);
+		double z = (Math.random()+2.5);
+		System.out.println("A number between 2.5-3.5: "+z);
+		double egg = (Math.random()*575+14);
+		System.out.println("A number between 14-589: "+egg);
 	}
 }
