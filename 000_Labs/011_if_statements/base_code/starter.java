9e2c69ee20c1733cf0639d820407198766d4cd54
sillyguy-sillyguy <asan8563@stu.gusd.net>
2026-09-21 21:37:10 +0000

    Ignite the long lost world that you forgot

--- 000_Labs/011_if_statements/base_code/starter.java
+++ 000_Labs/011_if_statements/base_code/starter.java
@@ -9,6 +9,17 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		if(){}
+		int x = (int)(Math.random()*10);
+		int y = (int)(Math.random()*10);
+		System.out.println("The range is from 0 to 10");
+		System.out.println("The first variable is: " + x);
+		System.out.println("The second variable is: " + y);
+		if(x==y){
+			System.out.println("The variables are equal");
+		}
+		if(x!=y){
+			System.out.println("The variables are different");
+		}
+		
 	}
 }
