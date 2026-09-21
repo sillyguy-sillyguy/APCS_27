28e1ee74bd2287940b57f198bd24d067ca86a7ca
sillyguy-sillyguy <asan8563@stu.gusd.net>
2026-09-21 21:48:18 +0000

    All the suffering repeats again and again and again.

--- 000_Labs/012_if_statements_more/base_code/starter.java
+++ 000_Labs/012_if_statements_more/base_code/starter.java
@@ -7,8 +7,17 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please input your first number: ");
+		int x = sc.nextInt();
+		System.out.print("Please input your second number: ");
+		int y = sc.nextInt();
+		if(x==y){
+			System.out.println("Your numbers are the same!");
+		}
+		if(x!=y){
+			System.out.println("Your numbers are different!");
+		}
+
 	}
 }
