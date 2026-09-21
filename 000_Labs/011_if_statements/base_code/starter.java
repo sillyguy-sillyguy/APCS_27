a1f9497740410d7c9b4d52e1820e9cf680415e25
sillyguy-sillyguy <asan8563@stu.gusd.net>
2026-09-21 21:27:26 +0000

    Go find your spark

--- 000_Labs/011_if_statements/base_code/starter.java
+++ 000_Labs/011_if_statements/base_code/starter.java
@@ -9,6 +9,6 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		if(){}
 	}
 }
