f6dcc681937342071318038ebe42fe32e573eaee
sillyguy-sillyguy <asan8563@stu.gusd.net>
2026-09-23 21:22:37 +0000

    Consequential, feel the difference in your head.

--- 100_HW/HW3_Divisible/base_code/starter.java
+++ 100_HW/HW3_Divisible/base_code/starter.java
@@ -8,8 +8,48 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely.");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please enter an integer: ");
+		int num1 = sc.nextInt();
+		System.out.print("Please enter another integer: ");
+		int num2 = sc.nextInt();
+		System.out.println();
+		if(num1%2==0){
+			System.out.println(num1+" is divisible by 2!");
+		}
+		if(num1%2!=0){
+			System.out.println(num1+" is not divisible by 2!");
+		}
+		if(num1%3==0){
+			System.out.println(num1+" is divisible by 3!");
+		}
+		if(num1%4==0){
+			System.out.println(num1+" is divisible by 4!");
+		}
+		if(num1%5==0){
+			System.out.println(num1+" is divisible by 5!");
+		}
+		if((num1%3!=0)&&(num1%4!=0)&&(num1%5!=0)){
+			System.out.println(num1+" is not divisible by 3, 4, or 5!");
+		}
+		System.out.println();
+		if(num2%2==0){
+			System.out.println(num2+" is divisible by 2!");
+		}
+		if(num2%3==0){
+			System.out.println(num2+" is divisible by 3!");
+		}
+		if(num2%4==0){
+			System.out.println(num2+" is divisible by 4!");
+		}
+		if(num2%5==0){
+			System.out.println(num2+" is divisible by 5!");
+		}
+		if(num2%2!=0){
+			System.out.println(num2+" is not divisible by 2!");
+		}
+		if((num2%3!=0)&&(num2%4!=0)&&(num2%5!=0)){
+			System.out.println(num2+" is not divisible by 3, 4, or 5!");
+		}
 	}
 }
