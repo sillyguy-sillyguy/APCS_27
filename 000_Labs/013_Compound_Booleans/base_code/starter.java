2c985d00fce66cdc11b4e7851d708fb212b9c17e
sillyguy-sillyguy <asan8563@stu.gusd.net>
2026-09-23 21:06:02 +0000

    A nirvanic concept fought so hard yet never enough.

--- 000_Labs/013_Compound_Booleans/base_code/starter.java
+++ 000_Labs/013_Compound_Booleans/base_code/starter.java
@@ -7,8 +7,39 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please enter your first number: ");
+		int num1 = sc.nextInt();
+		System.out.print("Please enter your second number: ");
+		int num2 = sc.nextInt();
+		System.out.print("Please enter your third number: ");
+		int num3 = sc.nextInt();
+		if((num1>num2)&&(num1>num3)){
+			System.out.println("Your first number is the largest of the three!");
+			System.out.println("The number was "+num1+".");
+		}
+		if((num2>num1)&&(num2>num3)){
+			System.out.println("Your second number is the largest of the three!");
+			System.out.println("The number was "+num2+".");
+		}
+		if((num3>num1)&&(num3>num2)){
+			System.out.println("Your third number is the largest of the three!");
+			System.out.println("The number was "+num3+".");
+		}
+		if((num3<num1)&&(num3<num2)){
+			System.out.println("Your third number is the smallest of the three!");
+			System.out.println("The number was "+num3+".");
+		}
+		if((num2<num1)&&(num2<num3)){
+			System.out.println("Your second number is the smallest of the three!");
+			System.out.println("The number was "+num2+".");
+		}
+		if((num1<num3)&&(num1<num2)){
+			System.out.println("Your first number is the smallest of the three!");
+			System.out.println("The number was "+num2+".");
+		}
+
+
+
 	}
 }
