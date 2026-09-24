ebaef456ea290b48e2fd1d14b7b6f87ea28bcac4
sillyguy-sillyguy <asan8563@stu.gusd.net>
2026-09-24 21:24:18 +0000

    Feel the depths of your shadows engulf your own soul.

--- 000_Labs/014_else/base_code/starter.java
+++ 000_Labs/014_else/base_code/starter.java
@@ -8,8 +8,14 @@ import java.util.Random;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner(System.in);
+		int RNG1K = (int)(Math.random()*1000+1);
+		System.out.print("Pick a number between 1 - 1000: ");
+		int ans = sc.nextInt();
+		if(ans==RNG1K){
+			System.out.println("No way! You got it! Good pick!");
+		} else{
+			System.out.println("Your number wasn't the random number. The number was "+RNG1K+".");
+		}
 	}
 }
