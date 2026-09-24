3b9359eb911de84b3d5c0a32af22a360d30253e4
sillyguy-sillyguy <asan8563@stu.gusd.net>
2026-09-24 20:30:24 +0000

    Feel the void consuming you in your long lost hell

--- 100_HW/HW4_Guessing_Game/base_code/starter.java
+++ 100_HW/HW4_Guessing_Game/base_code/starter.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely.");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("The goal of the game is to guess a word with two hints!");
+
 	}
 }
